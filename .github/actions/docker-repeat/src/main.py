060ac556bda0701a09de98553f32189865948961
julenminer <31986030+julenminer@users.noreply.github.com>
2020-10-26 11:32:37 +0100

    Create docker's main.py

---
+++ .github/actions/docker-repeat/src/main.py
@@ -0,0 +1,12 @@
+import os
+
+# Get the input values
+text = os.environ["INPUT_INPUT_TEXT"]
+numOfRepeats = os.environ["INPUT_NUM_OF_REPEATS"]
+
+outputText = ""
+for i in range(numOfRepeats):
+  outputText += text
+
+# Set the output value
+print(f"::set-output name=output_text::{outputText}")
